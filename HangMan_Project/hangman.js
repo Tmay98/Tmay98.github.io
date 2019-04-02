c812576871cff68e85f070a8974e16a172714897
Tommy <twmay98@hotmail.com>
2019-04-02 13:45:23 -0700

    added chooseWord

--- HangMan_Project/hangman.js
+++ HangMan_Project/hangman.js
@@ -1,10 +1,21 @@
+var btns = []
+createButtons()
+
 function createButtons(){
-letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for (let i=0; i < 26; i++ ){
-        var letter = String.fromCharCode(i+65)
-        let btn = document.createElement("BUTTON")
-        btn.innerHTML = letter
-        btn.onclick = function(){console.log('button '+letters[i]+' was pressed')}
-        document.body.appendChild(btn)
+    for (let i=0; i < 26; i++) {
+        var letter = String.fromCharCode(i+65);
+        btns[i] = document.createElement("BUTTON");
+        btns[i].innerHTML = letter;
+        btns[i].onclick = function() {buttonclicked(btns[i])};
+        document.body.appendChild(btns[i]);
     }
+}
+
+function buttonclicked(button){
+    button.disabled = true;
+}
+
+function chooseWord(list_of_words){ 
+    random_index = Math.floor(Math.random()*list_of_words.length)
+    return list_of_words[random_index]
 }
